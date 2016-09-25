2d5055610b07072652e8f96be5365fa2811efe43
Matthew B. Little <matthewblittle@gmail.com>
2016-09-24 21:33:18 -0500

    Update finite_diff_press.m

--- MATLAB_Examples/Coursework_Assignments/MATLAB_Class/Plot_Simple_Pressure/finite_diff_press.m
+++ MATLAB_Examples/Coursework_Assignments/MATLAB_Class/Plot_Simple_Pressure/finite_diff_press.m
@@ -8,7 +8,7 @@
 %   This improves upon the pressure approximation from the function p_at_d.
 %
 %-----------------------------------------------------------------------------------------
-%  Author:  Matthew Little
+%  Author:  Matthew B. Little
 %  Date:    22 Apr 2010  
 %  Revised: 13 Sep 2016       
 %-----------------------------------------------------------------------------------------
